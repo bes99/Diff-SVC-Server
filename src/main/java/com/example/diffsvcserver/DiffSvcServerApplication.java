633b7acfc76153f7183b11ea259752f95e776db5
bes99 <86958447+bes99@users.noreply.github.com>
2023-07-20 20:40:41 +0900

    first commit

---
+++ src/main/java/com/example/diffsvcserver/DiffSvcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.diffsvcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffSvcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiffSvcServerApplication.class, args);
+    }
+
+}
